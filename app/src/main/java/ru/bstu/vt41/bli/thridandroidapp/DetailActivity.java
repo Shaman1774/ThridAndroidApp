e0288a756adfe8458b85f9436f04faeb9e803be8
Shaman <w.krasotka95@mail.ru>
2018-04-06 21:57:31 +0300

    Update DetailActivity.java

--- app/src/main/java/ru/bstu/vt41/bli/thridandroidapp/DetailActivity.java
+++ app/src/main/java/ru/bstu/vt41/bli/thridandroidapp/DetailActivity.java
@@ -11,10 +11,12 @@ public class DetailActivity extends AppCompatActivity implements DetailFragment.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        
+        //Достаём из интента сериализованные данные по ключу/id "post"
         post = (Post) getIntent().getSerializableExtra("post");
     }
-
+    
+    //Реализация метода интерфейса OnFragmentPostListener
     @Override
     public Post onFragmentPost() {
         return post;
